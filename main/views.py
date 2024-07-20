4bb1f8c8cefa96f12f7dcf6de3184cdaa9cec477
glaciemdeus <equesimmortal@gmail.com>
2024-07-20 15:47:04 +0300

    Add posgresql DB

--- main/views.py
+++ main/views.py
@@ -13,34 +13,33 @@ def index(request):
 
 
 def weather_info(request):
+    try:
+        if request.method == 'POST':
+    # Получаем данные из формы
+            city = request.POST.get('city')
+            weather_info=weather_find(city)
+            current_temperature = weather_info[0]
+            time = str(weather_info[1])
+            
+            
+            
+            user_object = UserRequestModel(city=city, current_temperature=current_temperature, time=time)
+            user_object.save()
+
+
+
+            context = {
+                "current_temperature": current_temperature,
+                "city": city,
+                "time": time,
+            }
+            
+            
     
-    if request.method == 'POST':
- # Получаем данные из формы
-        city = request.POST.get('city')
-        logger.info(city)
-        weather_info=weather_find(city)
-        current_temperature = weather_info[0]
-        time = str(weather_info[1])
-        logger.info(current_temperature)
+            return render(request, 'main/weather.html', context=context)
         
-        
-        
-        user_object = UserRequestModel(city=city, current_temperature=current_temperature, time=time)
-        user_object.save()
-
-
-
-        context = {
-            "current_temperature": current_temperature,
-            "city": city,
-            "time": time,
-        }
-        
-        
- 
-        return render(request, 'main/weather.html', context=context)
-    
-    else:
- # Если это не POST-запрос, отображаем форму
+        else:
+    # Если это не POST-запрос, отображаем форму
+            return render(request, 'main/index.html')
+    except:
         return render(request, 'main/index.html')
-
